cb9dd93246927bbe633970a8eb14ee40ba021466
faribarz <barzegarfariborz@gmail.com>
2020-03-09 00:23:27 +0330

    first commit

---
+++ realtors/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Realtor
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','phone','email', 'is_mvp')
+    list_editable = ('is_mvp',)
+    search_fields = ('name',)
+    list_display_links = ('id', 'name')
+    list_per_page = 25
+
+admin.site.register(Realtor,RealtorAdmin)
